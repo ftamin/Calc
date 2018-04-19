0418e6156752870ba0ed3bd0128a8a1f5e8dd401
Farindra Tamin <farindra.tamin@loopup.com>
2018-04-19 13:08:39 +0700

    1st commit

---
+++ C#/MyCalculator/Calc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCalculator
+{
+    public class Calc
+    {
+        public double Add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        public double Minus(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        public double Multiply(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        public double Divide(double value1, double value2)
+        {
+            if (value2 == 0)
+            {
+                throw new DivisionException("value2 must not be 0");
+            }
+            return value1 / value2;
+        }
+
+        public double SqrtRoot(double value)
+        {
+            return (Math.Sqrt(value));
+        }
+
+        public double ByTwo(double value)
+        {
+            return Divide(value, 2);
+        }
+
+        public double ByFour(double value)
+        {
+            return Divide(value, 4);
+        }
+    }
+
+    public class CalcException : Exception
+    {
+        public CalcException(string v) : base(v)
+        {
+        }
+    }
+
+    public class DivisionException : CalcException
+    {
+        public DivisionException(string v) : base (v)
+        {
+        }
+    }
+}
---
+++ C#/MyCalculatorUnitTest/CalculatorUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using MyCalculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculatorUnitTest
+{
+    [TestFixture]
+    public class CalculatorUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddTwoPositiveValues()
+        {
+            var calc = new Calc();
+            var result = calc.Add(1, 2);
+
+            Assert.AreEqual(result, 3); // result is as expected
+            Assert.AreNotEqual(result, 1); // result is not equal to the first argument
+            Assert.AreNotEqual(result, 2); // result is not equal to the second argument
+            Assert.Greater(result, 0); // result must be always positive (the total of 2 positive numbers are always a positive number)
+        }
+
+        [Test]
+        public void AddTwoNegativeValues()
+        {
+            var calc = new Calc();
+            var result = calc.Add(-1, -2);
+
+            Assert.AreEqual(result, -3); // result is as expected
+            Assert.AreNotEqual(result, -1); // result is not equal to the first argument
+            Assert.AreNotEqual(result, -2); // result is not equal to the second argument
+            Assert.Less(result, 0); // result must be always negative (the total of 2 negative numbers are always a negative number)
+        }
+    }
+}
